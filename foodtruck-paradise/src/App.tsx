e5b98862774738cb26286bf36f7ccbc65f874966
franking31 <frankpange157@icloud.com>
2025-12-09 16:51:55 +0100

    change

--- foodtruck-paradise/src/App.tsx
+++ foodtruck-paradise/src/App.tsx
@@ -8,14 +8,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
       <div className="App">
-      <BrowserRouter>
+    
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/menu" element={<MenuPage />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      
     </div>
   );
 }
