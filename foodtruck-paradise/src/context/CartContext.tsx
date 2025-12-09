4a9209060f9dca922f1468ecbcdcf91317e045da
WidadMass <masgharwidad2003@gmail.com>
2025-12-09 16:15:44 +0100

    dbeug  de Cartcontext

--- foodtruck-paradise/src/context/CartContext.tsx
+++ foodtruck-paradise/src/context/CartContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useReducer, ReactNode } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
+import { type ReactNode } from 'react';
 import { type CartItem } from '../types/cart';
 import { type MenuItem } from '../types/menu';
 import { type PromoCode, promoCodes } from '../data/promoCodes';
